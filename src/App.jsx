dec7ed4c2719f81ced15f3bca292c80bef3a56fc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-08 12:31:53 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header"
+
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Truffle and Tailwind</h4>
+    <div className="min-h-screen text-gray-900 dark:text-gray-300 dark:bg-[#212936]">
+      <Header />
     </div>
   )
 }
