d35207ded45ed91364358b40998557a763e76028
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-09 10:43:54 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./components/Banner"
 import Header from "./components/Header"
 
 const App = () => {
   return (
     <div className="min-h-screen text-gray-900 dark:text-gray-300 dark:bg-[#212936]">
       <Header />
+      <Banner />
     </div>
   )
 }
