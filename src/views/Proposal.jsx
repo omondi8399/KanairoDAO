2efc447fa870202546f0563a912cb759373432f9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-10 12:54:25 +0300

    Updated Views

--- src/views/Proposal.jsx
+++ src/views/Proposal.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProposalDetails from '../components/ProposalDetails'
+import Voters from '../components/Voters'
 
 const Proposal = () => {
   return (
     <>
     <ProposalDetails />
+    <Voters />
     </>
   )
 }
