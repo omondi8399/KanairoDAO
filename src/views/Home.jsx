f8cfab1712aecec2e2f28502d14eee37947cff9a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-10 10:23:05 +0300

    Added Views

---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner"
+import Proposals from "../components/Proposals"
+
+const Home = () => {
+  return (
+    <>
+        <Banner />
+        <Proposals />
+    </>
+  )
+}
+
+export default Home
